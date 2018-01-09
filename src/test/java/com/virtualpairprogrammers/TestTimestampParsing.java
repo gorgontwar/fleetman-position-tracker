c76dcbdee7b0a374daf91f40ff7b1b5b380a9372
gorgontwar <34781549+gorgontwar@users.noreply.github.com>
2018-01-09 11:56:50 -0800

    Update TestTimestampParsing.java

--- src/test/java/com/virtualpairprogrammers/TestTimestampParsing.java
+++ src/test/java/com/virtualpairprogrammers/TestTimestampParsing.java
@@ -1,27 +1,23 @@
-public class StringExample
-{	public static void main(String[] args)
-	{	String s1 = "Computer Science";
-		int x = 307;
-		String s2 = s1 + " " + x;
-		String s3 = s2.substring(10,17);
-		String s4 = "is fun";
-		String s5 = s2 + s4;
-		
-		System.out.println("s1: " + s1);
-		System.out.println("s2: " + s2);
-		System.out.println("s3: " + s3);
-		System.out.println("s4: " + s4);
-		System.out.println("s5: " + s5);
-		
-		//showing effect of precedence
-		
-		x = 3;
-		int y = 5;
-		String s6 = x + y + "total";
-		String s7 = "total " + x + y;
-		String s8 = " " + x + y + "total";
-		System.out.println("s6: " + s6);
-		System.out.println("s7: " + s7);
-		System.out.println("s8: " + s8);
+package com.virtualpairprogrammers;
+
+import org.junit.Test;
+
+public class TestTimestampParsing 
+{
+	@Test
+	public void timestampsParseCorrectly() 
+	{
+		// Format: EEE MMM dd HH:mm:ss zzz yyyy	
+		Position position = new Position("23", "21", "Monday January 09 16:15:00 +0000 2017");
+	
+		// would throw exception if invalid
 	}
+
+	@Test(expected=java.lang.RuntimeException.class)
+	public void invalidTimestampsFailCorrectly() 
+	{
+		// Format: EEE MMM dd HH:mm:ss zzz yyyy		
+		Position position = new Position("23", "21", "Monday January 75 aa:15:00 +0000 2017");
+	}
+	
 }
